437164c26da6017d642a23d9250ed9dc3053258b
joseph <rajoseph37@gmail.com>
2023-07-21 05:46:18 +0000

    solution

---
+++ src/main/java/com/example/helloworld/HelloworldController.java
@@ -0,0 +1,20 @@
+package com.example.helloworld;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloworldController{
+
+    @GetMapping("/")
+
+    public String getwelcome(){
+        return "Hello World";
+    }
+
+    @GetMapping("/name")
+
+    public String getName(){
+        return "Joseph Raj";
+    }
+}
